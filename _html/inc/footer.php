4e8f7bf3c824e8754425da9501c094dae55a627e
OhmMiee <jeraphan2000@gmail.com>
2024-03-19 18:16:09 +0700

    update: footer

--- _html/inc/footer.php
+++ _html/inc/footer.php
@@ -1,5 +1,171 @@
 <footer class="layout-footer">
-    <div class="container-lg">
-        Footer
+  <div class="footer-content lazy" data-bg="<?php echo $core_template; ?>/img/background/bg-footer.webp" data-bg-hidpi="<?php echo $core_template; ?>/img/background/bg-footer@2x.webp">
+    <div class="container">
+      <div class="footer-top">
+        <div class="row">
+          <div class="col-md col-left">
+            <div class="title">กรมวิทยาศาสตร์การแพทย์</div>
+            <div class="subtitle">กระทรวงสาธารณสุข</div>
+          </div>
+          <div class="col-auto col-right">
+            <div class="title">098 915 6809</div>
+            <div class="subtitle"><img src="<?php echo $core_template; ?>/img/icon/contact-icon-call.svg" alt="" class="icon"> Call Center</div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-middle">
+        <div class="row justify-content-between">
+          <div class="col-md-auto">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-address.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    กรมวิทยาศาสตร์การแพทย์<br>
+                    กระทรวงสาธารณสุข 88/7 บำราศนราดูร<br>
+                    ถ.ติวานนท์ ต.ตลาดขวัญ อ.เมือง จ.นนทบุรี 11000
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-telephone.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">ติดต่อสอบถามข้อมูล</span>
+                    02 589 9850 ถึง 8
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-fax.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">โทรสาร</span>
+                    02 591 5974
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-fax.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">อีเมล์</span>
+                    <span class="d-block">prdmsc@dmsc.mail.go.th</span>
+                    <span class="d-block">info@dmsc.mail.go.th</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-fax.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">ข้อมูลโดย ฝ่ายประชาสัมพันธ์ สำนักงานเลขานุการกรม</span>
+                    <span class="d-block">E-mail : prdmsc@dmsc.mail.go.th</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <div class="followus">
+              <div class="row no-gutters align-items-center">
+                <div class="col-auto">
+                  Follow us:
+                </div>
+                <div class="col">
+                  <div class="social">
+                    <ul class="item-list">
+                      <li>
+                        <a href="" class="link">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/old-typical-phone.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" class="link">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/facebook.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" class="link">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/twitter.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" class="link">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/line.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="row">
+          <div class="col-md">
+            <div class="policy">
+              <ul class="item-list">
+                <li><a href="" class="link">นโยบายและแนวปฏิบัติในการรักษาความมั่นคงปลอดภัยด้านสารสนเทศ</a></li>
+                <li><a href="" class="link">นโยบายเว็บไซต์</a></li>
+                <li><a href="" class="link">นโยบายการคุ้มครองข้อมูลส่วนบุคคล กรมวิทยาศาสตร์การแพทย์</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <a href="" class="link sitemap">แผนผังเว็บไซต์</a>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div class="footer-bar">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md">
+          <p class="copyright">Copyright © 2024 Department of Medical Sciences, Ministry of Public Health. All rights reserved.</p>
+        </div>
+        <div class="col-md-auto">
+          <div class="visitors">จำนวนผู้เข้าชม <div class="box">2,651,012</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
