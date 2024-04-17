4c6a36b3052b709bdde6856524685e3662c5a76a
Jzoroz9 <109639854+Jzoroz9@users.noreply.github.com>
2024-04-17 22:53:23 +0700

    update: contact main done

---
+++ _html/contact.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include('inc/metatag.php'); ?>
+  <?php include('inc/loadstyle.php'); ?>
+</head>
+
+<body>
+
+  <div class="global-container">
+    <?php include('inc/header.php'); ?>
+
+    <section class="layout-body">
+      <div class="default-header">
+        <div class="wrapper">
+          <div class="container">
+            <div class="breadcrumb-block">
+              <ol class="breadcrumb">
+                <li>
+                  <a href="#" class="link">
+                    <span class="icon">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="15.856" height="15.857" viewBox="0 0 15.856 15.857">
+                        <path id="home-3" d="M15.43,6.9h0L8.96.428A1.459,1.459,0,0,0,6.9.428L.43,6.893.424,6.9A1.459,1.459,0,0,0,1.4,9.386l.045,0H1.7v4.76a1.71,1.71,0,0,0,1.709,1.708H5.937a.465.465,0,0,0,.465-.465V11.661a.78.78,0,0,1,.779-.779H8.674a.78.78,0,0,1,.779.779v3.732a.465.465,0,0,0,.465.465h2.531a1.71,1.71,0,0,0,1.709-1.708V9.389H14.4A1.46,1.46,0,0,0,15.43,6.9Zm0,0" transform="translate(0)" fill="#fff" />
+                      </svg>
+                    </span>
+                    หน้าหลัก
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="link">
+                    ติดต่อเรา
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="link">
+                    ข้อมูลการติดต่อ
+                  </a>
+                </li>
+              </ol>
+            </div>
+            <h1 class="title">
+              ข้อมูลการติดต่อ
+            </h1>
+            <div class="graphic">
+              <div class="obj">
+                <img src="<?php echo $core_template; ?>/img/uploads/obj-banner-about.png" alt="obj-banner-about" class="lazy img-cover">
+              </div>
+            </div>
+          </div>
+        </div>
+        <figure class="cover">
+          <img src="<?php echo $core_template; ?>/img/static/banner.jpg" alt="" class="lazy img-cover">
+        </figure>
+      </div>
+      <?php include('inc/footer-contact.php'); ?>
+      <div class="default-body pt-lg-3 pt-0">
+        <div class="contact-map">
+          <div class="container">
+            <ul class="nav">
+              <li>
+                <a href="#nav-01" data-toggle="tab" class="link active">Google map</a>
+              </li>
+              <li>
+                <a href="#nav-02" data-toggle="tab" class="link">Graphic map</a>
+              </li>
+            </ul>
+            <div class="tab-content">
+              <div class="tab-pane fade active show" id="nav-01">
+                <div class="iframe-container">
+                  <iframe class="responsive-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.5103212852896!2d100.55647707509134!3d13.808366986589444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29c4418406901%3A0xfe6bbac4647e2f5!2sNeighbouring%20Countries%20Economic%20Development%20Cooperation%20Agency%2C%20NEDA!5e0!3m2!1sen!2sth!4v1713361950661!5m2!1sen!2sth" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+              </div>
+              <div class="tab-pane fade" id="nav-02">
+                <figure class="cover">
+                  <img src="<?php echo $core_template; ?>/img/static/graphic-map.jpg" alt="" class="img-cover">
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contact-section">
+          <div class="contact-center">
+            <div class="container">
+              <div class="whead">
+                <h2 class="title">ส่วนกลาง</h2>
+              </div>
+              <div class="swiper">
+                <div class="swiper-wrapper">
+                  <?php for ($i = 1; $i <= 6; $i++) { ?>
+                    <div class="swiper-slide">
+                      <div class="item">
+                        <div class="contact-card">
+                          <div class="head">สำนักงานเลขานุการกรม</div>
+                          <div class="body">
+                            <div class="desc">
+                              สำนักงานเลขานุการกรม กรมวิทยาศาสตร์การแพทย์
+                              <br>
+                              88/7 บำราศนราดูร ถ.ติวานนท์ ต.ตลาดขวัญ อ.เมือง จ.นนทบุรี 11000
+                              <br>
+                              โทรศัพท์กลาง : 0-2951-0000 | ฝ่ายประชาสัมพันธ์ : ต่อ 99017,99081
+                              <br>
+                              โทรสาร: 0-2591-1707 | E-mail: prdmsc@dmsc.mail.go.th Map
+                            </div>
+                            <div class="action">
+                              <span class="text">Google map</span>
+                              <a href="" class="link">
+                                ดูแผนที่
+                                <span class="icon">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="14.25" height="14.25" viewBox="0 0 14.25 14.25">
+                                    <g id="Icon_ionic-ios-arrow-dropright" data-name="Icon ionic-ios-arrow-dropright" transform="translate(-3.375 -3.375)">
+                                      <path id="Path_25" data-name="Path 25" d="M14.609,10.175a.664.664,0,0,1,.935,0l3.268,3.278a.66.66,0,0,1,.021.911l-3.22,3.23a.66.66,0,1,1-.935-.932l2.737-2.778-2.805-2.778A.653.653,0,0,1,14.609,10.175Z" transform="translate(-5.661 -3.389)" fill="#2ab170" />
+                                      <path id="Path_26" data-name="Path 26" d="M3.375,10.5A7.125,7.125,0,1,0,10.5,3.375,7.124,7.124,0,0,0,3.375,10.5Zm1.1,0a6.035,6.035,0,1,1,1.768,4.261A5.977,5.977,0,0,1,4.471,10.5Z" fill="#2ab170" />
+                                    </g>
+                                  </svg>
+                                </span>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <?php } ?>
+                </div>
+                <div class="swiper-pagination"></div>
+              </div>
+            </div>
+          </div>
+          <div class="contact-center">
+            <div class="container">
+              <div class="whead">
+                <h2 class="title">ส่วนภูมิภาค</h2>
+              </div>
+              <div class="swiper">
+                <div class="swiper-wrapper">
+                  <?php for ($i = 1; $i <= 6; $i++) { ?>
+                    <div class="swiper-slide">
+                      <div class="item">
+                        <div class="contact-card">
+                          <div class="head">สำนักงานเลขานุการกรม</div>
+                          <div class="body">
+                            <div class="desc">
+                              สำนักงานเลขานุการกรม กรมวิทยาศาสตร์การแพทย์
+                              <br>
+                              88/7 บำราศนราดูร ถ.ติวานนท์ ต.ตลาดขวัญ อ.เมือง จ.นนทบุรี 11000
+                              <br>
+                              โทรศัพท์กลาง : 0-2951-0000 | ฝ่ายประชาสัมพันธ์ : ต่อ 99017,99081
+                              <br>
+                              โทรสาร: 0-2591-1707 | E-mail: prdmsc@dmsc.mail.go.th Map
+                            </div>
+                            <div class="action">
+                              <span class="text">Google map</span>
+                              <a href="" class="link">
+                                ดูแผนที่
+                                <span class="icon">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="14.25" height="14.25" viewBox="0 0 14.25 14.25">
+                                    <g id="Icon_ionic-ios-arrow-dropright" data-name="Icon ionic-ios-arrow-dropright" transform="translate(-3.375 -3.375)">
+                                      <path id="Path_25" data-name="Path 25" d="M14.609,10.175a.664.664,0,0,1,.935,0l3.268,3.278a.66.66,0,0,1,.021.911l-3.22,3.23a.66.66,0,1,1-.935-.932l2.737-2.778-2.805-2.778A.653.653,0,0,1,14.609,10.175Z" transform="translate(-5.661 -3.389)" fill="#2ab170" />
+                                      <path id="Path_26" data-name="Path 26" d="M3.375,10.5A7.125,7.125,0,1,0,10.5,3.375,7.124,7.124,0,0,0,3.375,10.5Zm1.1,0a6.035,6.035,0,1,1,1.768,4.261A5.977,5.977,0,0,1,4.471,10.5Z" fill="#2ab170" />
+                                    </g>
+                                  </svg>
+                                </span>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <?php } ?>
+                </div>
+                <div class="swiper-pagination"></div>
+              </div>
+            </div>
+          </div>
+          <div class="contact-bg"></div>
+        </div>
+        <div class="contact-service">
+          <div class="container">
+            <div class="whead">
+              <h2 class="title">บริการ</h2>
+            </div>
+            <div class="contact-service-list">
+              <?php for ($i = 1; $i <= 9; $i++) { ?>
+                <div class="item">
+                  <div class="row no-gutters align-items-start mb-3">
+                    <div class="col-auto">
+                      <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22.741" height="25" viewBox="0 0 22.741 25">
+                          <g id="customer-service" transform="translate(-1.13)">
+                            <path id="Path_452614" data-name="Path 452614" d="M21.561,18.14l-.028-.033a9.6,9.6,0,0,0-5.318-3.171A6.618,6.618,0,0,0,18.6,11.765h1.675a1.7,1.7,0,0,0,1.7-1.7v-.5a9.559,9.559,0,1,0-19.118,0v.5a1.7,1.7,0,0,0,1.7,1.7H6.177A6.619,6.619,0,0,0,8.62,14.974a9.587,9.587,0,0,0-5.153,3.133l-.028.033A9.293,9.293,0,0,0,1.13,24.265V25H23.87v-.735a9.293,9.293,0,0,0-2.309-6.125ZM12.39,14.692a5.123,5.123,0,0,1-4.624-2.928H11.44V10.294H7.324a5.117,5.117,0,1,1,5.066,4.4Zm8.119-4.629a.231.231,0,0,1-.231.231h-1.34a6.574,6.574,0,0,0,0-1.471h1.541c.022.242.034.487.034.735ZM12.421,1.471A8.1,8.1,0,0,1,20.2,7.353H18.591a6.587,6.587,0,0,0-12.4,0H4.638a8.1,8.1,0,0,1,7.782-5.882ZM4.564,10.294a.231.231,0,0,1-.231-.231v-.5c0-.248.012-.493.034-.735h1.48a6.574,6.574,0,0,0,0,1.471ZM2.634,23.529a7.936,7.936,0,0,1,1.928-4.44l.028-.033a8.266,8.266,0,0,1,6.353-2.879h3.114a8.266,8.266,0,0,1,6.353,2.88l.028.033a7.937,7.937,0,0,1,1.928,4.44Z" fill="#fff" />
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="title">สอบถามข้อมูลทั่วไป</div>
+                    </div>
+                  </div>
+                  <div class="tel">
+                    <span class="fw-bold">เบอร์โทรศัพท์ :</span>
+                    <a href="tel:0 2589 9850-7" class="link">0 2589 9850-7</a>
+                    <br>
+                    Operator กด 0, ฝ่ายประชาสัมพันธ์
+                    <br>
+                    ต่อ 99081
+                  </div>
+                </div>
+              <?php } ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include('inc/footer.php'); ?>
+  </div>
+
+  <?php include('inc/loadscript.php'); ?>
+
+  <script>
+    var contactMap = new Swiper(".contact-center .swiper", {
+      // slidesPerView: "auto",
+      slidesPerView: 2,
+      // freeMode: true,
+      spaceBetween: 23,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        767: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        // 1199: {
+        //   slidesPerView: 2,
+        //   spaceBetween: 15,
+        // }
+      }
+    });
+  </script>
+</body>
+
+</html>
---
+++ _html/inc/footer-contact.php
@@ -0,0 +1,151 @@
+<footer class="layout-footer footer-contact">
+    <div class="container">
+      <div class="footer-top">
+        <div class="row">
+          <div class="col-md col-left" data-aos="fade-left">
+            <div class="title">กรมวิทยาศาสตร์การแพทย์</div>
+            <div class="subtitle">กระทรวงสาธารณสุข</div>
+          </div>
+          <div class="col-auto col-right" data-aos="fade-right">
+            <div class="title"><a href="tel:098 915 6809" class="link">098 915 6809</a></div>
+            <div class="subtitle"><img src="<?php echo $core_template; ?>/img/icon/contact-icon-call.svg" alt="" class="icon"> Call Center</div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-middle">
+        <div class="row justify-content-lg-between">
+          <div class="col-md-auto" data-aos="fade-up">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-address.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    กรมวิทยาศาสตร์การแพทย์<br>
+                    กระทรวงสาธารณสุข 88/7 บำราศนราดูร<br>
+                    ถ.ติวานนท์ ต.ตลาดขวัญ อ.เมือง จ.นนทบุรี 11000
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto" data-aos="fade-up">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-telephone.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">ติดต่อสอบถามข้อมูล</span>
+                    <a href="tel:02 589 9850" class="link">02 589 9850 ถึง 8</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto" data-aos="fade-up">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-fax.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">โทรสาร</span>
+                    <a href="fax:02 591 5974" class="link">02 591 5974</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto" data-aos="fade-up">
+            <div class="contact">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-email.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">อีเมล์</span>
+                    <a href="mailto:prdmsc@dmsc.mail.go.th" class="link"><span class="d-block">prdmsc@dmsc.mail.go.th</span></a>
+                    <a href="mailto:info@dmsc.mail.go.th" class="link"><span class="d-block">info@dmsc.mail.go.th</span></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Force next columns to break to new line -->
+          <div class="w-100 p-0 d-xl-block d-none"></div>
+          <div class="col-md" data-aos="fade-up">
+            <div class="contact pt-md-3">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img src="<?php echo $core_template; ?>/img/icon/contact-icon-fax.svg" alt="" class="icon">
+                </div>
+                <div class="col">
+                  <p class="desc">
+                    <span class="d-block">ข้อมูลโดย ฝ่ายประชาสัมพันธ์ สำนักงานเลขานุการกรม</span>
+                    <span class="d-block">E-mail : <a href="prdmsc@dmsc.mail.go.th" class="link">prdmsc@dmsc.mail.go.th</a></span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w-100 p-0 d-lg-none"></div>
+          <div class="col-lg col-sm-auto" data-aos="fade-up">
+            <div class="followus pt-md-3">
+              <div class="row no-gutters align-items-center justify-content-md-end">
+                <div class="col-auto">
+                  Follow us:
+                </div>
+                <div class="col-auto">
+                  <div class="social">
+                    <ul class="item-list">
+                      <li>
+                        <a href="" class="link tele" title="Telephone">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/old-typical-phone.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.facebook.com/DMScNews/" class="link fb" title="Facebook" target="_blank">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/facebook.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://twitter.com/DMScNew" class="link tw" title="X" target="_blank">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/twitter.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.youtube.com/user/DMSCLive" class="link yt" title="YouTube" target="_blank">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/youtube.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://line.me/R/ti/p/%40009xhxof" class="link line" title="Line" target="_blank">
+                          <div class="rounded-0">
+                            <img src="<?php echo $core_template; ?>/img/icon/line.svg" alt="" class="icon">
+                          </div>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
