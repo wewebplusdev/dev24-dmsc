32576967436ea6ce21d069204b44fb24f62890e9
wittayapong <wittayapong.t@wewebplus.com>
2024-04-23 14:00:27 +0700

    Update Code Backoffice cmsg By Ruck V4

--- weadmin/mod_cmsg/loadEditContant.php
+++ weadmin/mod_cmsg/loadEditContant.php
@@ -671,7 +671,7 @@ foreach ($listAuthen as $key => $value) {
                </td>
             </tr>
          </table>
-         <br class="TypeDownload" <?php if($valTypeC != 2){ echo "style='display:none;'"; } ?>/>
+         <br class="TypeDownload" <?php if($valTypeC != 1){ echo "style='display:none;'"; } ?>/>
          <table width="96%" border="0" cellspacing="0" cellpadding="0" align="center" class="tbBoxViewBorder TypeDetail" <?php if($valTypeC != 1){ echo "style='display:none;'"; } ?>>
             <tr>
                <td colspan="7" align="left" valign="middle" class="formTileTxt tbBoxViewBorderBottom">
