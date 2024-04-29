9fc0df0fac1c46012d998a5666013fa5b3b3f035
3Melody <plugeiei2003@gmail.com>
2024-04-29 10:09:15 +0700

    create-file
    content-structure

---
+++ _html/content-structure.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('inc/metatag.php'); ?>
+    <?php include('inc/loadstyle.php'); ?>
+
+    <style>
+        
+    </style>
+</head>
+
+<body class="global-container">
+    <?php include('inc/header.php'); ?>
+
+
+
+    <?php include('inc/footer.php'); ?>
+</body>
+
+</html>
