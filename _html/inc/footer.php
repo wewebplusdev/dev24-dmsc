ffca859090fd2437c5f922b6c28f24988ee9be96
Jzoroz9 <109639854+Jzoroz9@users.noreply.github.com>
2024-05-02 17:01:49 +0700

    update: contact-form

--- _html/inc/footer.php
+++ _html/inc/footer.php
@@ -188,7 +188,7 @@
   </div>
 </footer>
 
-<div class="sitmap-full show">
+<div class="sitmap-full">
   <div class="close">
     <svg xmlns="http://www.w3.org/2000/svg" width="37.993" height="37.993" viewBox="0 0 37.993 37.993">
       <g id="Group_882" data-name="Group 882" transform="translate(-11556.55 148.45)">
