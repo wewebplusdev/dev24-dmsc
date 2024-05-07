234f7e0b07fa26fc0e52c26fe5ce50525def93d9
Phanuwat Sutthiwong <playgroundclub_1@hotmail.com>
2024-05-07 14:11:17 +0700

    Revert "Merge pull request #14 from wewebplusdev/Jin"
    This reverts commit 58d674e3b1a80d078edc6e1200302f3ecaa7201a, reversing
    changes made to 949b5e5b53eb2fc8325e1603bef0f368a5cca0d9.

--- _html/content-template.php
+++ _html/content-template.php
@@ -29,13 +29,18 @@
                 </li>
                 <li>
                   <a href="#" class="link">
-                    ค้นหา
+                    เกี่ยวกับเรา
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="link">
+                    ประวัติความเป็นมา
                   </a>
                 </li>
               </ol>
             </div>
             <h1 class="title">
-              ค้นหา
+              เกี่ยวกับหน่วยงาน
             </h1>
             <div class="graphic">
               <div class="obj">
@@ -48,70 +53,13 @@
           <img src="<?php echo $core_template; ?>/img/static/banner.jpg" alt="" class="lazy img-cover">
         </figure>
       </div>
-      <!-- ck editor -->
-      <div class="document-download-list">
-            <div class="container">
-              <div class="whead">
-                <h2 class="title text-center">เอกสารดาวน์โหลด</h2>
-              </div>
-              <div class="swiper">
-                <div class="swiper-wrapper">
-                  <?php for ($i = 1; $i <= 5; $i++) { ?>
-                    <div class="swiper-slide">
-                      <div class="item">
-                        <a href="" class="link">
-                          <div class="row gutters-20 aling-items-start">
-                            <div class="col-auto">
-                              <span class="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="57.688" height="48.202" viewBox="0 0 57.688 48.202">
-                                  <g id="download-3" transform="translate(-6.5 -48.832)">
-                                    <path id="Path_451708" data-name="Path 451708" d="M10.726,125.151A3.225,3.225,0,0,1,7.5,121.925v-32.1a2.017,2.017,0,0,1,2.017-2.017H16.9" transform="translate(0 -33.724)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <line id="Line_8910" data-name="Line 8910" x1="21.759" transform="translate(19.836 91.427)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451709" data-name="Path 451709" d="M41.478,189.617H36.29a3.26,3.26,0,0,0,3.217-3.275V162.429a2.017,2.017,0,0,1,2.017-2.017H79.637a2.017,2.017,0,0,1,2.017,2.017v8.682" transform="translate(-25.564 -98.19)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451710" data-name="Path 451710" d="M111.291,344.116H101.207a1.12,1.12,0,0,1-1.12-1.12v-3.051a1.12,1.12,0,0,1,1.12-1.121h10.084a1.12,1.12,0,0,1,1.12,1.121V343A1.12,1.12,0,0,1,111.291,344.116Z" transform="translate(-82.213 -256.611)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <line id="Line_8911" data-name="Line 8911" x2="5.897" transform="translate(17.874 66.215)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <line id="Line_8912" data-name="Line 8912" x2="8.874" transform="translate(17.874 70.137)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451711" data-name="Path 451711" d="M91.37,62.222V51.849a2.017,2.017,0,0,1,2.017-2.017h25.329" transform="translate(-74.472)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451712" data-name="Path 451712" d="M370.42,49.832h2.954a2.017,2.017,0,0,1,2.017,2.017V62.222" transform="translate(-322.255)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <line id="Line_8913" data-name="Line 8913" x2="26.044" transform="translate(21.994 54.066)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <line id="Line_8914" data-name="Line 8914" x2="26.044" transform="translate(21.994 57.988)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451713" data-name="Path 451713" d="M288.607,258.371a12.1,12.1,0,1,1,12.1,12.1A12.1,12.1,0,0,1,288.607,258.371Z" transform="translate(-249.609 -174.433)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451714" data-name="Path 451714" d="M362.7,375.1v2.091a.9.9,0,0,1-.9.9H353.82a.9.9,0,0,1-.9-.9V375.1" transform="translate(-306.719 -288.822)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <line id="Line_8915" data-name="Line 8915" y2="6.161" transform="translate(51.093 78.612)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                    <path id="Path_451715" data-name="Path 451715" d="M374.3,343.128l2.195,1.963a.448.448,0,0,0,.6,0l2.195-1.963" transform="translate(-325.699 -260.432)" fill="none" stroke="#29b171" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
-                                  </g>
-                                </svg>
-                              </span>
-                            </div>
-                            <div class="col">
-                              <div class="desc">
-                                หน่วยทดสอบความชำนาญทั้งส่วนกลางและส่วนภูมิภาค
-                                ซึ่งการสมัครเข้าร่วม
-                              </div>
-                              <div class="type-file">
-                                <ul class="item-list">
-                                  <li>
-                                    <strong>ประเภทไฟล์ :</strong> <span>.pdf</span>
-                                  </li>
-                                  <li>
-                                    <strong>ขนาดไฟล์ :</strong> <span>25 Mb</span>
-                                  </li>
-                                  <li>
-                                    <strong>ดาวน์โหลด :</strong> <span>15 ครั้ง</span>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </a>
-                      </div>
-                    </div>
-                  <?php } ?>
-                </div>
-                <div class="swiper-pagination"></div>
-              </div>
-            </div>
-          </div>
+      <div class="default-body">
+        <!-- ck editor -->
+        <div class="editor-content">
+
+        </div>
+        <!-- ck editor -->
+      </div>
     </section>
 
     <?php include('inc/footer.php'); ?>
@@ -119,31 +67,9 @@
 
   <?php include('inc/loadscript.php'); ?>
 
+  <script>
+   
+  </script>
 </body>
 
 </html>
-
-<script>
-    var downloadSwiper = new Swiper(".document-download-list .swiper", {
-        // slidesPerView: "auto",
-        slidesPerView: 2,
-        // freeMode: true,
-        spaceBetween: 40,
-        watchSlidesProgress: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 2,
-            },
-            768: {
-                slidesPerView: 3,
-            },
-            1200: {
-                slidesPerView: 2,
-            }
-        }
-    });
-</script>
