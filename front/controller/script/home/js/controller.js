b48bed53b10d49d582bdc960271e5f7020029aae
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2024-05-08 17:42:21 +0700

    Revert "controller"
    This reverts commit f5d6c78fbc67b8f70e4487e5434c5ca9d18e6856.

--- front/controller/script/home/js/controller.js
+++ front/controller/script/home/js/controller.js
@@ -70,11 +70,4 @@ $('.services-filter').on('click', async function(){
             console.error("Error occurred:", error);
         }
     }
-
-
-    let value = eval('obj.' + propName); 
-
-    let func = Function('obj' + propName);
-
-    location.href = 'javascript:void(0)'; 
 });
