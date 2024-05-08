60df8bd8e1f878de7a748995b500beaaaa5752fe
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2024-05-08 17:42:24 +0700

    Revert "update controller"
    This reverts commit b19a039ff86b332d36e115ac80dfac21dc9b1906.

--- front/controller/script/home/js/controller.js
+++ front/controller/script/home/js/controller.js
@@ -3,13 +3,12 @@ $('.services-filter').on('click', async function(){
 
     if ($(this).hasClass('active')) {
         $(this).removeClass('active');
-    } else {
+    }else{
         $(this).addClass('active');
     }
-
-    let array_gid = [];
-    $('.services-filter.active').each(function() {
-        array_gid.push($(this).data('id'));
+    let array_gid = new Array;
+    $('.services-filter.active').map((key,value) => {
+        array_gid.push($(value).data('id'));
     });
 
     if (array_gid.length > 0 || true) {
@@ -29,45 +28,40 @@ $('.services-filter').on('click', async function(){
                 "limit": 15,
             }),
         };
+        const result = await $.ajax(settings);
 
-        try {
-            const result = await $.ajax(settings);
-
-            if (result?.code === 1001 && result?._numOfRows > 0) {
-                let strHTML = ``;
-                result?.item?.list.forEach((value) => {
-                    let url = (value.url && value.url !== '#' && value.url !== "") ? value.url : "javascript:void(0);";
-                    let target = (value.url && value.url !== '#' && value.url !== "") ? value.target : "_self";
-                    strHTML += `
-                    <div class="swiper-slide">
-                        <div class="item">
-                            <a href="${url}" class="link" target="${target}">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="thumbnail">
-                                            <figure class="contain">
-                                                <img src="${value.pic?.pictures || ''}"
-                                                    alt="${value.pic?.subject || ''}" class="thumb-img lazy">
-                                                <img src="${value.pic2?.pictures || ''}"
-                                                    alt="${value.pic2?.subject || ''}" class="thumb-hover lazy">
-                                            </figure>
-                                        </div>
-                                        <h5 class="title">${value.subject || ''}</h5>
+        if (result?.code === 1001 && result?._numOfRows > 0) {
+            let strHTML = ``;
+            result?.item?.list.map((value) => {
+                let url = (value.url != '#' && value.url != "") ? value.url : "javascript:void(0);";
+                let target = (value.url != '#' && value.url != "") ? value.target : "_self";
+                strHTML += `
+                <div class="swiper-slide">
+                    <div class="item">
+                        <a href="${url}" class="link" target="${target}">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="thumbnail">
+                                        <figure class="contain">
+                                            <img src="${value.pic.pictures}"
+                                                alt="${value.pic.subject}" class="thumb-img lazy">
+                                            <img src="${value.pic2.pictures}"
+                                                alt="${value.pic2.subject}" class="thumb-hover lazy">
+                                        </figure>
                                     </div>
+                                    <h5 class="title">${value.subject}</h5>
                                 </div>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                     </div>
-                    `;
-                });
-                $('#service-append').empty();
-                $('#service-append').append(strHTML);
-            } else {
-                $('#service-append').empty();
-            }
-            reload_swiper();
-        } catch (error) {
-            console.error("Error occurred:", error);
+                </div>
+                `;
+            });
+            $('#service-append').empty();
+            $('#service-append').append(strHTML);
+        }else{
+            $('#service-append').empty();
         }
+        reload_swiper();
     }
 });
