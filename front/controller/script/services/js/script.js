236deaba23a0572d4885556ccb8576e2f4669c54
Arisa <arisa.p@wewebplus.com>
2024-05-10 14:57:12 +0700

    services\js\script.js swiper

--- front/controller/script/services/js/script.js
+++ front/controller/script/services/js/script.js
@@ -1,27 +1,26 @@
+let serviceSwiper = reload_swiper();
+
 function reload_swiper() {
-    (function () {
-        new Swiper(".service-category-list .swiper", {
-            slidesPerView: 5,
-            watchSlidesProgress: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+    return new Swiper(".service-category-list .swiper", {
+        slidesPerView: 5,
+        watchSlidesProgress: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 2,
             },
-            breakpoints: {
-                0: {
-                    slidesPerView: 2,
-                },
-                768: {
-                    slidesPerView: 3,
-                },
-                1024: {
-                    slidesPerView: 4,
-                },
-                1200: {
-                    slidesPerView: 5,
-                },
-            }
-        });
-    })();
+            768: {
+                slidesPerView: 3,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+            1200: {
+                slidesPerView: 5,
+            },
+        }
+    });
 }
-reload_swiper();
