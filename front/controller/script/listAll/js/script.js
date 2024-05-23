0948c1af325dd739b56acb1862db2714df1289f5
phoovanon2 <159443812+sonkanit1201@users.noreply.github.com>
2024-05-23 12:20:30 +0700

    edit script

--- front/controller/script/listAll/js/script.js
+++ front/controller/script/listAll/js/script.js
@@ -24,5 +24,5 @@ $(".filter-form").on("click", async function () {
   $("#filter-form").submit();
 });
 $(".select-filter").on("change", async function () {
-  submit();
+  $("#filter-form").submit();
 });
--- front/controller/script/services/js/script.js
+++ front/controller/script/services/js/script.js
@@ -28,5 +28,5 @@ $(".filter-form").on("click", async function () {
   $("#filter-form").submit();
 });
 $(".select-filter").on("change", async function () {
-  submit();
+  $("#filter-form").submit();
 });
