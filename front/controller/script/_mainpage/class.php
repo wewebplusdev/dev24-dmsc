97bfba9c26197c31adf6dff342e39f1526089e2c
Phanuwat Sutthiwong <playgroundclub_1@hotmail.com>
2024-05-27 14:03:24 +0700

    fix

--- front/controller/script/_mainpage/class.php
+++ front/controller/script/_mainpage/class.php
@@ -35,9 +35,9 @@ abstract class Controller
         {
             if ($_CORE_ENV == 'DEV') {
                 // $this->urlAPI =  'http://192.168.101.39:4040/service-api/v1';
-                $this->urlAPI =  'http://192.168.1.112:4040/service-api/v1';
+                // $this->urlAPI =  'http://192.168.1.112:4040/service-api/v1';
                 // $this->urlAPI =  'http://api.wewebplus.com:4040/service-api/v1';
-                // $this->urlAPI =  'http://45.136.253.128:4040/service-api/v1';
+                $this->urlAPI =  'http://45.136.253.128:4040/service-api/v1';
             } elseif ($_CORE_ENV == 'PROD') {
                 $this->urlAPI =  'http://192.168.200.146:4040/service-api/v1';
             } else {
