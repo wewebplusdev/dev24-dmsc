60222a0f5d8bfceb7a58891a9bf1ca5534528c91
Phanuwat Sutthiwong <playgroundclub_1@hotmail.com>
2024-05-28 10:25:49 +0700

    cache

--- front/libs/config.php
+++ front/libs/config.php
@@ -73,8 +73,8 @@ $path_template = array(
 );
 
 $lastModify = "?u=" . date("YdmHis");
-$LastVersionCache = "?v=".date("YdmHis");
-// $LastVersionCache = "?v=6";
+// $LastVersionCache = "?v=".date("YdmHis");
+$LastVersionCache = "?v=10";
 
 ##  Config inc-file
 $incfile['header'] = "inc/inc-header.tpl";
